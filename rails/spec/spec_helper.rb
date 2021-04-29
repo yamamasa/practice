8df9814a918aa0a34ceb8eaaa15ec683efd426e2
Masayoshi Yamamoto <fcqdy819@gmail.com>
2021-04-30 01:27:50 +0900

    guardでrspec動くようにした

--- rails/spec/spec_helper.rb
+++ rails/spec/spec_helper.rb
@@ -15,6 +15,18 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
 
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+SimpleCov.configure do
+  add_filter '/spec/'
+  add_filter '/app/jobs/application_job.rb'
+  add_filter '/app/mailers/application_mailer.rb'
+  add_filter '/app/models/application_record.rb'
+  add_filter '/app/channels/application_cable/channel.rb'
+  add_filter '/app/channels/application_cable/connection.rb'
+end
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
